e3ca04ffaeeaa74375f2ee31ba6947356514b07e
GustavoKuhn <gustavowolgep@gmail.com>
2019-02-28 01:03:59 -0300

    mais cod

--- Trab1_Perceptron/v0.py
+++ Trab1_Perceptron/v0.py
@@ -1,26 +1,36 @@
 import csv
+import random
+import numpy as np
 
 arqDados = '/sdcard/git/trabsRedesNeurais/Trab1_Perceptron/dados.csv'
 arqTrein = '/sdcard/git/trabsRedesNeurais/Trab1_Perceptron/treinamento.csv'
- 
-x0 = []
-x1 = []
-x2 = []
 
-with open(arqTrein) as csvDataFile:
-    csvReader = csv.reader(csvDataFile)
+dadosTreino = []
+dadosTeste = []
+
+def importCsv(treino, teste):
+    csvReader = csv.reader(open(treino))
     for row in csvReader:
-        x0.append(row[0])
-        x1.append(row[1])
-        x2.append(row[2])
+        dadosTreino.append(row)
+    dadosTreino.pop(0)                            ##remove a 1st linha (cabecalho)
+    
+    csvReader = csv.reader(open(teste))
+    for row in csvReader:
+        dadosTeste.append(row)
+    dadosTeste.pop(0)
 
-#print(x0)
-#print(x1)
-#print(x2)
+def normaliza():
+    maior = 0
+    #for row in dadosTeste:
+    for i in range(len(dadosTeste)):
+        rowMax = max(dadosTeste[i][:2])
+        if rowMax > maior:
+            maior = rowMax
+    
+    return maior
 
 class Neuronio():
-    def __init__(self, g, teta, w):
-        self.g=g
+    def __init__(self, teta, w):
         self.teta=teta
         self.w=w
     
@@ -29,19 +39,39 @@ class Neuronio():
         for i in range(0,2):
             res=res + self.w[i]*x[i]
         res=res - self.teta
-        return G(res)
-    
+        return self.G(res)
+        
     def G(self, u):
         if u<0:
             return -1
         else:
             return 1
-            
-class rede():
+
+class Perceptron():
+    def __init__(self):
+        self.n = 0.01             #taxa de aprendizagem
+        self.w = [random.randrange(0.1, 1, 0.1), random.randrange(0.1, 1, 0.1), random.randrange(0.1, 1, 0.1)]
+        self.teta = -1            #random.randrange(0.1, 1, 0.1)
+        self.neuronio = Neuronio(self.teta, self.w)
     
+    def treino(dados):
+        denovo = True
+        count = 0
+        
+        while (denovo):
+            res = self.neuronio.out(dadosTreino[count][:2])     #passa a primeira e a segunda coluna da linha count
+            
+            err = dadosTreino[count][2] - res
+            
+            
 
 def main():
-    print "hello"
+    importCsv('dados.csv', 'treinamento.csv')
+    
+    print(dadosTreino)
+    print(dadosTeste)
+    
+    print("hello")
 
 
 
