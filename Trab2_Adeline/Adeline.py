876e79dd33bac2a6c86bee42c5e392257889b887
Gustavo <gustavowogep@gmail.com>
2019-03-13 00:36:41 -0300

    correcao da quarta coluna (entrada)

--- Trab2_Adeline/Adeline.py
+++ Trab2_Adeline/Adeline.py
@@ -6,14 +6,14 @@ import pandas as pd
 df = pd.read_excel('./371889-tarefa02_treino.xls')                  #le o arquivo xls
 treino = np.array(df)                                               #cria uma array com os dados lidos do excel
 
-d = treino[:,3]                                                     #copia a terceira coluna de 'treino' para 'd'
+d = treino[:,4]                                                     #copia a terceira coluna de 'treino' para 'd'
 norma = np.amax(treino)                                             #pega o maior valor de 'treino'
 treino = np.dot(1/norma, treino)                                    #divive o vetor 'treino' pelo seu maior valor, deixando seus valores entre 0 e 1
 
 linhas = len(treino)                                                #numero de linhas no vetor 'treino'
-x = np.ones([linhas, 4])                                            #cria uma martiz de uns com tamanho 30x4
+x = np.ones([linhas, 5])                                            #cria uma martiz de uns com tamanho 30x4
 x = x * -1                                                          #multiplica a matriz 'x' por -1 (deixa todos os valores iguais a -1)
-x[:, 1:4] = treino[:,0:3]                                           #copia para as 3 ultimas colunas de 'x' as 3 primeiras de 'treino'
+x[:, 1:5] = treino[:,0:4]                                           #copia para as 3 ultimas colunas de 'x' as 3 primeiras de 'treino'
 
 dTreino = d[ 0:25]                                                  
 xTreino = x[ 0:25]                                                  #separa 70% para treino (70% de 30 = 21)
@@ -27,9 +27,9 @@ operacao  = np.array(df)                                            #cria uma ar
 operacao = np.dot(1/norma, operacao)                                #divide pela mesmo valor que os dados de trino foram divididos
 
 linhas = len(operacao)                                              #numero de linhas no vetor 'operacao'
-xOperacao = np.ones([linhas, 4])                                    #cria uma martiz de uns com tamanho 10x4
+xOperacao = np.ones([linhas, 5])                                    #cria uma martiz de uns com tamanho 10x4
 xOperacao = xOperacao * -1                                          #multiplica xOperacao por -1
-xOperacao[:, 1:4] = operacao[:,0:3]                                 #
+xOperacao[:, 1:5] = operacao[:,0:4]                                 #
 
 
 def G(u):
@@ -42,13 +42,12 @@ def Eqw(w):
     return 1
 
 def Treinar():                                                      #treina o Perceptron e retorna os pesos e n de epocas
-    w = np.random.random(4)*2-1
+    w = np.random.random(5)*2-1
     n = 0.0025
     eps = 1e-6
     
     epoca = 0
     
-    E = 0
     Eant = 0
     Eatual = 0
     
@@ -65,15 +64,16 @@ def Treinar():                                                      #treina o Pe
             u = np.dot(w.T, xTreino[k])                             # u = w0*x0 + w1*x1 + w2*x2 + w3*x3
             
             #w0 = w0 + n*(d0 - y)*x0; w1 = w1 + n*(d1 - y)*x1 ... 
-            var = n*(dTreino[k] - u)                            #fator de correcao dos pesos
-            w = w + np.dot(var, xTreino[k])                     #multiplica o vetor 'x' por 'var' e soma no vetor 'w'
+            var = n*(dTreino[k] - u)                                #fator de correcao dos pesos
+            w = w + np.dot(var, xTreino[k])                         #multiplica o vetor 'x' por 'var' e soma no vetor 'w'
             
             er = er + ((dTreino[k] - u)**2)
         Eatual = er/len(dTreino)
         
         dE = Eatual - Eant
         if dE < 0:
-            dE = -dE
+            dE = -dE                                                #modulo
+            
         if dE > eps:
             erro = True
         else:
