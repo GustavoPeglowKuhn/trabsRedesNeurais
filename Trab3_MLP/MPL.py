780d7140901e554c7c323690c5b691f65bbc83df
GustavoKuhn <gustavowolgep@gmail.com>
2019-04-04 14:34:26 -0200

    Update MPL.py
    todas saidas convergem para 0.6

--- Trab3_MLP/MPL.py
+++ Trab3_MLP/MPL.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 n = 0.1
 eps = 1e-6
@@ -13,7 +14,7 @@ nNeuronios = [10, nOut]
 
 
 
-dados = np.array(pd.read_excel('373922-Teste_projeto_1_MLP.xls'))
+dados = np.array(pd.read_excel('373925-Treinamento_projeto_1_MLP.xls'))
 nAmostras = len(dados)
 d = dados[:, 3]
 x = np.ones([len(dados), len(dados[0])])
@@ -34,41 +35,95 @@ def Y(_w, _x, nI):
     _y = np.zeros(nI+1)
     _y[0] = -1
     for i in range(len(_l)):
-        _y[i+1] = math.tanh(_l[i])
+        _y[i+1] = g(_l[i])
     return _l, _y
+
+def Em(_d, _y, _n):
+	res = 0
+	for i in range(_n):
+		res = res + ((_d[i]-_y[i])**2)
+	#res = res = ((_d[:]-_y[:])**2).sum()
+	return .5*res/_n
 
 ############################### treino #
 w = [0, np.random.random([nNeuronios[0], nIn+1])*2-1, np.random.random([nOut, nNeuronios[0]+1])*2-1]
 l = [0,               np.zeros(nNeuronios[0]),   np.zeros(nNeuronios[1])]
 y = [np.zeros(nIn+1), np.zeros(nNeuronios[0]+1), np.zeros(nNeuronios[1]+1)]
 
-wList = []
-wList.append(w.copy())
+w0 = w.copy()
 
-############################## Laço ##
-iteracao = 0
+saidas = np.zeros(nAmostras)
 
-############################## Forward ###
-y[0] = x[iteracao]
+epocas = 0
+continuar = True
 
-l[1], y[1] = Y(w[1], y[0], len(l[1]))
-l[2], y[2] = Y(w[2], y[1], len(l[2]))
-#y[2][1:]
+E = Eant = 0
+Elist = []
 
-############################## Backward ###
-sigma2 = (d[iteracao]-y[2][1:]) * dg(l[2])
-wAnt = w.copy()
-w[2] = wAnt[2] + n*(sigma2*y[2][1:])
+while continuar:
+	Eant = E
+	for amostra in range(nAmostras):
+		############################## Forward ###
+		y[0] = x[amostra]
+
+		l[1], y[1] = Y(w[1], y[0], len(l[1]))
+		l[2], y[2] = Y(w[2], y[1], len(l[2]))
+		saidas[amostra] = y[2][1:]
+
+		############################## Backward ###
+		wAnt = w.copy()
+        
+		sigma2 = (d[amostra]-y[2][1]) * dg(l[2])
+		w[2][0,:] = wAnt[2][0,:] + n*sigma2*y[1][:]
+
+		#de camada 2 para a 1
+
+		dg1 = np.zeros(nNeuronios[0])
+		for i in range(nNeuronios[0]):
+			dg1 = dg(l[1][i])
+		
+		sigma1 = -w[2][0,1:]*sigma2
+		
+		for i in range(nNeuronios[0]):
+			w[1][i,:] = wAnt[1][i,:] + sigma1[i]*n*x[amostra,:]
+	
+	E = Em(d, saidas, nAmostras)
+	Elist.append(E)
+
+	epocas = epocas+1
+	continuar = abs(Eant-E)>eps
 
 
+###############################################################################
+############################## Teste #
+
+dadosTeste = np.array(pd.read_excel('373922-Teste_projeto_1_MLP.xls'))
+nTeste = len(dadosTeste)
+dt = dadosTeste[:, 3]
+xt = np.ones([len(dadosTeste), len(dadosTeste[0])])
+xt[:, 0] = xt[:, 0]*-1
+xt[:, 1:] = dadosTeste[:, :3]
+
+lt = [0,               np.zeros(nNeuronios[0]),   np.zeros(nNeuronios[1])]
+yt = [np.zeros(nIn+1), np.zeros(nNeuronios[0]+1), np.zeros(nNeuronios[1]+1)]
+
+res = [dt, np.zeros(nTeste)]
+
+for amostra in range(nTeste):
+	yt[0] = x[amostra]
+
+	lt[1], yt[1] = Y(w[1], yt[0], len(lt[1]))
+	lt[2], yt[2] = Y(w[2], yt[1], len(lt[2]))
+	
+	res[1][amostra] = yt[2][1]
+	
 
 
+print(np.matrix(res))
 
-
-
-
-
-
+plt.plot(Elist)
+plt.ylabel('Elist')
+plt.show()
 
 
 
