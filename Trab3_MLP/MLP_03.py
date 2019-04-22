ef214fcc1dc2657c3c57d4509432643cbe296981
GustavoKuhn <gustavowolgep@gmail.com>
2019-04-22 18:41:24 -0200

    add momentum ao 2 e inicio do 3

--- Trab3_MLP/MLP_03.py
+++ Trab3_MLP/MLP_03.py
@@ -2,29 +2,33 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def importData(file, _nIn, _nOut):
-	dados = np.array(pd.read_excel(file))
-	d = dados[:, _nIn:].copy()
-	x = dados[:, :_nIn+1].copy()
-	x[:, _nIn:] = np.ones([len(dados),1])*-1
-	return x, d, len(dados), _nIn, _nOut
 
-###leituraa dos dados do ex1
-#xTreino, dTreino, sTreino, nIn, nOut = importData('373925-Treinamento_projeto_1_MLP.xls', 3, 1)
-#xTeste,  dTeste,  sTeste,  nIn, nOut = importData('373922-Teste_projeto_1_MLP.xls', 3, 1)
+print('###########################################################################')
+print('###########################################################################')
+print('#                                                                         #')
+print('#                            Apenas rascunho                              #')
+print('#                                                                         #')
+print('###########################################################################')
+print('###########################################################################')
 
-###leituraa dos dados do ex2
-xTreino, dTreino, sTreino, nIn, nOut = importData('373926-Treinamento_projeto_2_MLP.xls', 4, 3)
-xTeste,  dTeste,  sTeste,  nIn, nOut  = importData('373923-Teste_projeto_2_MLP.xls', 4, 3)
+
+def importData(file):
+	x = np.array(pd.read_excel(file))
+	return x, len(x)
+
+###leituraa dos dados do ex3
+xTreino, sTreino = importData('373928-Treinamento_projeto_3_MLP.xls')
+xTeste,  sTeste  = importData('373924-Teste_projeto_3_MLP.xls')
 
 ###caracteristicas da rede
 aprendizado = 0.1
-momentum = 0.9
-Emax = 1e-6
+momentum = 0.8
+Emax = 0.5e-6
 epocasMax = 500		#limite de epocas para o treino
 
+nIn, nOut =  5, 1
 camadas = 2         #1 ocultas e 1 de saida
-n = [15, nOut]
+n = [10, nOut]
 
 def g(x):
 	return 1. / (1. + np.exp(-x))	
@@ -33,7 +37,7 @@ def dg(x):
 	return g(x)*(1.-g(x))
 
 
-x, d, size = xTreino, dTreino, sTreino
+x, size = xTreino, sTreino
 ############################################################
 ############  Treino
 w = [np.random.random([n[0], nIn+1])*2-1, np.random.random([n[1], n[0]+1])*2-1]
@@ -57,12 +61,12 @@ while (abs(Eant-E)>Emax and epocas < epocasMax):
 	E = 0
 
 
-	for i in range(size):	
+	for i in range(size-nIn):	
 		wa2 = wa1.copy()
 		wa1 =   w.copy()
 		
 		############################## Forward ###
-		l[0] = np.dot(w[0], x[i])
+		l[0] = np.dot(w[0], x[i:i+nIn])
 		for j in range(n[0]): 
 			y[0][j] = g(l[0][j])
 		y[0][n[0]] = -1
@@ -104,10 +108,10 @@ while (abs(Eant-E)>Emax and epocas < epocasMax):
 
 ############################################################
 ############  Teste
-x, d, size = xTeste,  dTeste,  sTeste
+x, size = xTeste,  sTeste
 
 res = np.zeros([size, 2*nOut])
-res[:, :nOut] = d
+#res[:, :nOut] = d
 E = 0
 l = [np.zeros(n[0]),   np.zeros(n[1])]
 y = [np.zeros(n[0]+1), np.zeros(n[1])]
