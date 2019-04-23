dc24924c748121b9ecf6c65f0a7dcf8b594aeca5
GustavoKuhn <gustavowolgep@gmail.com>
2019-04-23 16:53:05 -0200

    sempre converge para 0.3

--- Trab3_MLP/MLP_03.py
+++ Trab3_MLP/MLP_03.py
@@ -3,15 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-print('###########################################################################')
-print('###########################################################################')
-print('#                                                                         #')
-print('#                            Apenas rascunho                              #')
-print('#                                                                         #')
-print('###########################################################################')
-print('###########################################################################')
-
-
 def importData(file):
 	x = np.array(pd.read_excel(file))
 	return x, len(x)
@@ -22,16 +13,16 @@ xTeste,  sTeste  = importData('373924-Teste_projeto_3_MLP.xls')
 
 ###caracteristicas da rede
 aprendizado = 0.1
-momentum = 0.8
+momentum = 0.5
 Emax = 0.5e-6
 epocasMax = 500		#limite de epocas para o treino
 
 nIn, nOut =  5, 1
 camadas = 2         #1 ocultas e 1 de saida
-n = [10, nOut]
+n = [nIn+5, nOut]
 
 def g(x):
-	return 1. / (1. + np.exp(-x))	
+	return 1. / (1. + np.exp(-x))
 
 def dg(x):
 	return g(x)*(1.-g(x))
@@ -61,47 +52,55 @@ while (abs(Eant-E)>Emax and epocas < epocasMax):
 	E = 0
 
 
-	for i in range(size-nIn):	
+	for i in range(size-(nIn+1)):	
 		wa2 = wa1.copy()
 		wa1 =   w.copy()
 		
+		xt = x[i:i+nIn+1].T.copy()
+		xt[0, nIn] = -1
+		d = x[i+nIn]
+
 		############################## Forward ###
-		l[0] = np.dot(w[0], x[i:i+nIn])
-		for j in range(n[0]): 
+		l[0] = np.dot(w[0], xt.T)
+		for j in range(n[0]):
 			y[0][j] = g(l[0][j])
 		y[0][n[0]] = -1
 		l[1] = np.dot(w[1], y[0])
-		for j in range(n[1]): 
-			y[1][j] = g(l[1][j])
+		j = 0	#for j in range(n[1]): 
+		y[1][j] = g(l[1][j])
 
 		############################## Backward ###
-		for j in range(n[1]):
-			s[1][j] = (d[i][j] - y[1][j]) * dg(l[1][j])
-			wn[1][j] = w[1][j] + (aprendizado * s[1][j] * y[0][j])
+		j = 0
+		s[1][j] = (d - y[1][j]) * dg(l[1][j])
+		wn[1][j] = w[1][j] + (aprendizado * s[1][j] * y[0][j])
 
-		for j in range(n[0]):
-			s[0][j] = np.dot(s[1], w[1][:, j]) * dg(l[0][j])
-			wn[0][j] = w[0][j] + (aprendizado * s[0][j] * x[i])
+		j = 0		#for j in range(n[0]):
+		s[0][j] = np.dot(s[1], w[1][:, j]) * dg(l[0][j])
+		wn[0][j] = w[0][j] + (aprendizado * s[0][j] * xt)
 		
 		w[0] = wn[0] + momentum * (wa1[0] - wa2[0])
 		w[1] = wn[1] + momentum * (wa1[1] - wa2[1])
 		#w = wn
 	
-	for i in range(size):
+	for i in range(size-(nIn+1)):
+		xt = x[i:i+nIn+1].T.copy()
+		xt[0, nIn] = -1
+		d = x[i+nIn]
+
 		############################## Forward ###
-		l[0] = np.dot(w[0], x[i])
+		l[0] = np.dot(w[0], xt.T)
 		for j in range(n[0]): 
 			y[0][j] = g(l[0][j])
 		y[0][n[0]] = -1
+		
 		l[1] = np.dot(w[1], y[0])
-		for j in range(n[1]): 
-			y[1][j] = g(l[1][j])
+		j = 0	#for j in range(n[1]): 
+		y[1][j] = g(l[1][j])
 			
-		er = 0
-		for j in range(nOut):
-			er = er + ((d[i][j] - y[1][j])**2)
+		j = 0
+		er = ((d - y[1][j])**2)
 		E = E + 0.5*er
-	E = E/size
+	E = E/(size-(nIn+1))
 	Elist.append(E)
 	epocas = epocas +1
 
@@ -110,37 +109,42 @@ while (abs(Eant-E)>Emax and epocas < epocasMax):
 ############  Teste
 x, size = xTeste,  sTeste
 
-res = np.zeros([size, 2*nOut])
+res = np.zeros([size-(nIn+1), 2])
 #res[:, :nOut] = d
 E = 0
+eam = 0
 l = [np.zeros(n[0]),   np.zeros(n[1])]
 y = [np.zeros(n[0]+1), np.zeros(n[1])]
-for i in range(size):
+for i in range(size-(nIn+1)):
+	xt = x[i:i+nIn+1].T.copy()
+	xt[0, nIn] = -1
+	d = x[i+nIn]
+
+	res[i, 0] = d
+
 	############################## Forward ###
-	l[0] = np.dot(w[0], x[i])
+	l[0] = np.dot(w[0], xt.T)
 	for j in range(n[0]): 
 		y[0][j] = g(l[0][j])
 	y[0][n[0]] = -1
+	
 	l[1] = np.dot(w[1], y[0])
-	for j in range(n[1]): 
-		y[1][j] = g(l[1][j])
-	for j in range(n[1]):
-		if y[1][j] < 0.5:
-			res[i, nOut+j] = 0
-		else:
-			res[i, nOut+j] = 1
-	#res[i, nOut:] = y[1]
-	er = 0
-	for j in range(n[1]):
-		er = er + ((d[i][j] - y[1][j])**2)
-		E = E + 0.5*er 
+	j = 0	#for j in range(n[1]): 
+	y[1][j] = g(l[1][j])
+	res[i, 1] = y[1][0]
+	
+	eam = eam + abs(res[i, 0] - res[i, 1])
+	
+	j = 0
+	er = ((d - y[1][j])**2)
 	E = E + 0.5*er
-E = E/size
+E = E/(size-(nIn+1))
+eam = eam/(size-(nIn+1))
 
 
 print(np.matrix(res))
 
-print('Eqm: '+str(E)+'    epocas: '+str(epocas))
+print('Eqm: '+str(E)+'       Eam: '+str(eam)+'    epocas: '+str(epocas))
 
 plt.plot(Elist)
 plt.ylabel('Elist')
